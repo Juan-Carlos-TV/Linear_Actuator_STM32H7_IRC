a4f86c48628cf98b6a0db1d9bbc12fa10e3495d9
ianyan11 <iandelagg@gmail.com>
2021-11-22 14:21:46 -0600

    Lectura encoder

--- CM7/Core/Src/main.c
+++ CM7/Core/Src/main.c
@@ -400,12 +400,12 @@ static void MX_TIM2_Init(void)
   htim2.Init.Period = 4294967295;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  sConfig.EncoderMode = TIM_ENCODERMODE_TI1;
-  sConfig.IC1Polarity = TIM_ICPOLARITY_FALLING;
+  sConfig.EncoderMode = TIM_ENCODERMODE_TI12;
+  sConfig.IC1Polarity = TIM_ICPOLARITY_RISING;
   sConfig.IC1Selection = TIM_ICSELECTION_DIRECTTI;
   sConfig.IC1Prescaler = TIM_ICPSC_DIV1;
   sConfig.IC1Filter = 10;
-  sConfig.IC2Polarity = TIM_ICPOLARITY_FALLING;
+  sConfig.IC2Polarity = TIM_ICPOLARITY_RISING;
   sConfig.IC2Selection = TIM_ICSELECTION_DIRECTTI;
   sConfig.IC2Prescaler = TIM_ICPSC_DIV1;
   sConfig.IC2Filter = 10;
@@ -594,11 +594,18 @@ static void MX_GPIO_Init(void)
 void StartTask1(void *argument)
 {
   /* USER CODE BEGIN 5 */
-  /* Infinite loop */
-  for(;;)
-  {
-    osDelay(1);
-  }
+	  uint8_t MSG[50] = {'\0'};
+	  uint32_t steps;
+	  HAL_TIM_Encoder_Start(&htim2, TIM_CHANNEL_ALL);
+	  /* Infinite loop */
+	  for(;;)
+	  {
+		steps =  __HAL_TIM_GET_COUNTER(&htim2);
+		sprintf(MSG, "Encoder Ticks = %d\n\r", steps);
+		osMessageQueuePut(positionQueueHandle, &steps, 4, 50);
+		HAL_UART_Transmit(&huart3, MSG, sizeof(MSG), 100);
+	    osDelay(5);
+	  }
   /* USER CODE END 5 */
 }
 
